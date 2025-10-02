fb767b14e01b3e02be5abae200b3a38eef46af97
Lucas <lucascarrizoruggeri2004@gmail.com>
2025-10-02 11:53:47 +0200

    Previous moments before changing donebutton style

--- Scripts/UI_Items/TaskWidgetContainer.gd
+++ Scripts/UI_Items/TaskWidgetContainer.gd
@@ -20,7 +20,8 @@ func set_container() -> void:
 		task.new_tag_created.connect(_update_tasks_select_tag_button)
 		vbox.add_child(task)
 		task_widget_counter += 1
-
+	
+	_update_tasks_stylebox()
 
 func _update_tasks_select_tag_button() -> void:
 	for task in vbox.get_children():
@@ -36,6 +37,8 @@ func _update_tasks_stylebox() -> void:
 		
 		var task_done = Database.is_task_done(task._get_task_id())
 		if task_done:
-			task._set_style_task_inactive()
+			task._set_style_task_done()
+			task.done_button.button_pressed = true
 		else:
-			task._set_style_task_active()
+			task._set_style_task_not_done()
+			task.done_button.button_pressed = false
